69094a4f3c5efcd3edc4b43ec40969bffbbdb82a
Alex <375336849110@tut.by>
2016-02-08 00:02:57 +0300

    Добавлены фамилии студентов курса JD01

---
+++ src/akhmelev/Main.java
@@ -0,0 +1,10 @@
+package akhmelev;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Hello World!");
+    }
+}
