92d40e3d292be1b25d82f191ab8c78535e403a51
Ivanov <ivan@ivan.ru>
2016-02-08 07:31:52 +0300

    add domain it.by

--- src/by/it/akhmelev/Demo.java
+++ src/by/it/akhmelev/Demo.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Demo {
 
     public static void main(String[] args) throws IOException {
-        System.out.print("Hello World!");
+        System.out.print("Hello World !!!");
     }
 }
