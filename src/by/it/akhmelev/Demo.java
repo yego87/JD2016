33cb4710310d92faa7d686e19cd33469b455a5c7
Alex <375336849110@tut.by>
2016-02-09 11:47:53 +0300

    Добавлены права доступа

--- src/by/it/akhmelev/Demo.java
+++ src/by/it/akhmelev/Demo.java
@@ -3,7 +3,7 @@ package by.it.akhmelev;
 import java.io.IOException;
 
 public class Demo {
-
+    //ок
     public static void main(String[] args) throws IOException {
         System.out.print("Hello World !!!");
     }
