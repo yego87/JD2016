3f45be6ab8b0fefa40929eee8e358983b323f91c
Alex <375336849110@tut.by>
2016-02-09 15:02:14 +0300

    Удаление тестовых файлов (1.txt)

--- src/by/it/akhmelev/Demo.java
+++ src/by/it/akhmelev/Demo.java
@@ -3,7 +3,7 @@ package by.it.akhmelev;
 import java.io.IOException;
 
 public class Demo {
-    //master ok8
+    //master ok-11
     public static void main(String[] args) throws IOException {
         System.out.print("Hello World !!!");
     }
