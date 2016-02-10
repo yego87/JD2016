a8551d6ae9cf3b6cb1999303e79234aaa9629587
Kniazeu Artsem <artsem.k@gmail.com>
2016-02-10 11:55:34 +0300

    Вывод чисел, длина которых меньше (больше) средней и их длину

--- src/by/it/knyazev/Less_f1t2/id_3/Main.java
+++ src/by/it/knyazev/Less_f1t2/id_3/Main.java
@@ -3,22 +3,16 @@ package by.it.knyazev.Less_f1t2.id_3;
 /**
  * Created by Artsem Kniazeu on 10.02.2016.
  */
- 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        InputStreamReader inputStreamReader=new InputStreamReader(System.in);
-        BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
-        String text = bufferedReader.readLine();
-        bufferedReader.close();
-
-        //String text = "1 22 333 4444 55555 666666";
-
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
         String[] massiv = new String[text.length()];
 
         massiv = text.split(" ");
