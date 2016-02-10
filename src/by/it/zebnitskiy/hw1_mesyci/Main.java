4e1eb36d2901fad17f955a675e1d75e0264b954a
Зебницкий Максим <Alukard,max@gmail.com>
2016-02-10 12:30:02 +0300

    задание В  месяцы Зебницкий

--- src/by/it/zebnitskiy/hw1_mesyci/Main.java
+++ src/by/it/zebnitskiy/hw1_mesyci/Main.java
@@ -12,7 +12,6 @@ public class Main {
     if (D  >  0){
         x1 = (-b + Math.sqrt(D)) / (2 * a);
         x2 = (-b - Math.sqrt(D)) / (2 * a);
-
         System.out.println("Число действительных корней!");
         System.out.println("x1 = " + x1);
         System.out.println("x2 = " + x2);
