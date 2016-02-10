ead1b10075aa53c6923d10f1fbf09ecdc185850b
Телушко <vladimir.telushko@gmail.com>
2016-02-10 19:30:33 +0300

    Телушко Все задания по массивам и СЛАУ....

--- src/by/it/telushko/jd01_04/Main.java
+++ src/by/it/telushko/jd01_04/Main.java
@@ -1,7 +1,5 @@
 package by.it.telushko.jd01_04;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +10,23 @@ public class Main {
         InOut.arrayPrint(x);
         System.out.println("Проверка");
         InOut.arrayPrint(Util.arrayMulVector(m, x));
+
+        //поиск определителя
+        
+        m=Util.gaussForward(m);
+        double det=m[0][0];
+        for (int diag = 1; diag <m.length ; diag++) {
+            det=det*m[diag][diag];
+        }
+        System.out.println("определитель матрицы коэффициентов: "+det);
+
+        //поиск обратной матрицы
+
+        InOut.arrayPrint2D(Util.reverseMatrix(m));
+
+        //проверка ОМ
+        System.out.println("проверка");
+        InOut.arrayPrint2D(Util.arrayMulVector(m,Util.reverseMatrix(m)));
     }
 }
 
