1a3a237166f2cd6a6dde39bb136d1d3f92361256
Mikhail Darashchonak <mikhail@darashchonak.by>
2016-02-12 00:04:18 +0300

    JD01-05 Task3 completed

--- src/by/it/dorostchenok/jd01_05/math/Task3.java
+++ src/by/it/dorostchenok/jd01_05/math/Task3.java
@@ -7,21 +7,14 @@ public class Task3 {
     public static void printTable(){
 
         double x = 3.567;
-        int tableWidth = 20;
+
+        System.out.println("┏━━━━━━━┳━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ param ┃        value       ┃");
+        System.out.println("┣━━━━━━━╋━━━━━━━━━━━━━━━━━━━━┫");
         for (double a = 3.5; a <= 25.5; a += 3.75){
             double y = (1.0 / tan(pow(x, 3))) + 2.24 * a * x;
-            //if()
-            //System.out.println("┃ " + a +"\t┃ " + y + "┃" );
-            String s = String.format("%1s%-12s%-1s%-15s%5s", "┃", a, "┃", y, "┃");
-            System.out.println(s);
+            System.out.println(String.format("%-2s%-6s%-2s%-19s%-20s", "┃", a, "┃", y, "┃"));
         }
-        System.out.println("");
-        System.out.println("┏━━━━━━━┳━━━━━━━┓");
-        System.out.println("┃ param ┃ value ┃");
-        System.out.println("┗━━━━━━━┻━━━━━━━┛");
-
-        String s = String.format("%1s%8s%9s%10s", "one", "two", "three", "four");
-        System.out.println(s);
-
+        System.out.println("┗━━━━━━━┻━━━━━━━━━━━━━━━━━━━━┛");
     }
 }
