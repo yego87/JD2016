dae1b778594fae68db5e3df23a85cd28e78e128a
Predkel <slouch@bk,ru>
2016-02-12 12:29:03 +0300

    Начата работа со String

---
+++ src/by/it/predkel/JD06String/Main.java
@@ -0,0 +1,18 @@
+package by.it.predkel.JD06String;
+
+
+import static by.it.predkel.JD06String.Insert5th7thLetter.*;
+
+/**
+ * Created by user_2 on 12.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String fraze="У лукоморья дуб зелёный и блабла";
+        String changed= insert5th7thLetter(fraze);
+
+
+    }
+
+
+}
