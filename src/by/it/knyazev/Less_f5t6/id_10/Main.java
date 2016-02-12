88986845e86e5726ece6740829da3ab89b53fe3f
Kniazeu Artsem <artsem.k@gmail.com>
2016-02-12 12:04:24 +0300

    #Test

--- src/by/it/knyazev/Less_f5t6/id_10/Main.java
+++ src/by/it/knyazev/Less_f5t6/id_10/Main.java
@@ -1,18 +1,20 @@
 package by.it.knyazev.Less_f5t6.id_10;
-
-
 /**
  * Created by Artsem Kniazeu on 10.02.2016.
  */
- 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String... args) {
+        String regex = "\\w{1,}@\\w{1,}";
+        String s = "asdad@asd, 1asdad@asd, asdasdad@asd";
+        Pattern p2 = Pattern.compile(regex);
+        Matcher m2 = p2.matcher(s);
+        while (m2.find()){
+            System.out.println("e-mail:"+m2.group());
+        }
     }
 
 }
