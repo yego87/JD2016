160c9b0e8c913e60f59bbc1c72fe973e02e36c6a
Kniazeu Artsem <artsem.k@gmail.com>
2016-02-12 10:00:32 +0300

    #new

---
+++ src/by/it/knyazev/Less_f5t6/id_10/Main.java
@@ -0,0 +1,18 @@
+package by.it.knyazev.Less_f5t6.id_10;
+
+
+/**
+ * Created by Artsem Kniazeu on 10.02.2016.
+ */
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
