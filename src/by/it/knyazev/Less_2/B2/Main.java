70edc49438cf9f0e97b6ad3285cf4e927951b980
Kniazeu Artsem <artsem.k@gmail.com>
2016-02-13 11:46:05 +0300

    Вывести числа от 1 до k в виде матрицы NxN

---
+++ src/by/it/knyazev/Less_2/B2/Main.java
@@ -0,0 +1,25 @@
+package by.it.knyazev.Less_2.B2;
+
+import java.util.Scanner;
+
+/**
+ * Created by Artsem Kniazeu on 13.02.2016.
+ */
+public class Main {
+    public static void main(String... args){
+        String s = "1 2 3 4 5 6 7 8 9";
+        String[] arr = new String[s.length()];
+        arr = s.split(" ");
+
+        int f = 3; //ширина
+
+        int row = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            row++;
+            System.out.printf(arr[i] + " ");
+            if (0 == row%f){
+                System.out.println();
+            }
+        }
+    }
+}
