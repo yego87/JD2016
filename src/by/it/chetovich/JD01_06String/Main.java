71f47da768c8142fd2b615deada2e7a53fd94fc3
Yultos <chatovich@gmail.com>
2016-02-15 00:12:25 +0300

    Задания A-B String

--- src/by/it/chetovich/JD01_06String/Main.java
+++ src/by/it/chetovich/JD01_06String/Main.java
@@ -3,12 +3,9 @@ package by.it.chetovich.JD01_06String;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
- * Created by user_2 on 12.02.2016.
+ * JD01_06 String
  */
 public class Main {
     public static void main (String [] args) throws IOException {
@@ -16,9 +13,29 @@ public class Main {
         String text = reader.readLine();
         StringBuilder textB = new StringBuilder(text);
 
-        //Task1
-        Task1String task1 = new Task1String();
-        task1.changeLiterals(textB);
+        //TaskA1
+        //TaskA1String taskA1 = new TaskA1String();
+        //taskA1.changeLiterals(textB);
+
+        //TaskA2
+        //TaskA2String taskA2 = new TaskA2String();
+        //taskA2.CalculateWords(textB);
+
+        //Task A3
+        //TaskA3String taskA3 = new TaskA3String();
+        //taskA3.calcWordsWithConsonant(textB);
+
+        //Task B1
+        //TaskB1String taskB1 = new TaskB1String();
+        //taskB1.deleteWordsStartConsonant(textB);
+
+        //Task B2
+        //TaskB2String taskB2 = new TaskB2String();
+        //taskB2.sortTextBySentences(textB);
+
+        TaskB3String taskB3 = new TaskB3String();
+        taskB3.sortWordsByLetter(textB, "ì");
+
 
     }
 }
