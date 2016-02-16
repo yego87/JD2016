18b600dbc6a7e15aa4a45afbcf93039cc496ebe6
Kniazeu Artsem <artsem.k@gmail.com>
2016-02-16 21:15:12 +0300

    #Вариант 6 З_6 - ?

--- src/by/it/knyazev/Less_5/M6/Main.java
+++ src/by/it/knyazev/Less_5/M6/Main.java
@@ -4,4 +4,11 @@ package by.it.knyazev.Less_5.M6;
  * Created by Artsem Kniazeu on 15.02.2016.
  */
 public class Main {
+    public static void main(String... args){
+        // d=lg|k+6|
+        double x = -3;
+        double y = 2;
+        double f = 20;
+
+    }
 }
