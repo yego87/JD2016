c87a8a04cd1476b34a147d50b29de2f31324c3ad
Alex <375336849110@tut.by>
2016-02-17 01:55:44 +0300

    Комментарии в package by.it.chetovich.JD01_06StringTaskC2 (а то валит весь проект)

--- src/by/it/akhmelev/JD01_08/Dumb/Dumb.java
+++
@@ -1,7 +0,0 @@
-package by.it.akhmelev.JD01_08.Dumb;
-
-/**
- * Created by Alex on 16.02.2016.
- */
-public class Dumb {
-}
--- src/by/it/chetovich/JD01_06StringTaskC2/C2CreatingRandomStringLine.java
+++ src/by/it/chetovich/JD01_06StringTaskC2/C2CreatingRandomStringLine.java
@@ -1,6 +1,6 @@
 package by.it.chetovich.JD01_06StringTaskC2;
 
-import by.it.chetovich.JD01_06StringTaskC2.C2ChoosingWordByLength;
+//import by.it.chetovich.JD01_06StringTaskC2.C2ChoosingWordByLength;
 
 import java.util.Random;
 
@@ -21,7 +21,7 @@ public class C2CreatingRandomStringLine {
         for (int i = 0; i < 999; i++) { //delaem stroku pomen6e, million slov sli6kom dolgo zhadat))
             line += array[rand.nextInt(array.length)] +" ";
         }
-        line += C2ChoosingWordByLength.wordByLength(array, lastWordLength);
+       // line += C2ChoosingWordByLength.wordByLength(array, lastWordLength);
         //System.out.println(line);
     }
 
