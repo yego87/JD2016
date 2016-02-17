3a478c5df063aee0bd78250045806fb54db4b31b
yegor.veselov <yegor.veselov@gmail.com>
2016-02-17 11:30:57 +0300

    JD01_02 and other

---
+++ src/by/it/veselov/JD01_02/Task_1_A/Task_1_A.java
@@ -0,0 +1,46 @@
+package by.it.veselov.JD01_02.Task_1_A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by yegorveselov on 16.02.16.
+ */
+public class Task_1_A {
+
+            public static void main(String[] args) throws IOException {
+                InputStreamReader inputStreamReader=new InputStreamReader (System.in);
+                BufferedReader bufferedReader=new BufferedReader (inputStreamReader);
+                String rLine=bufferedReader.readLine();
+                bufferedReader.close();
+
+                String[] val=rLine.split(" ");
+
+
+            String max = val[0];
+            String min = val[0];
+            for (int i=0;i<val.length;i++)
+                if (max.length() < val[i].length()) { max=val[i];}
+                    System.out.println("самое длинное число: " + max + " длина которого " + max.length());
+                for ( int i=0;i>val.length;i++)
+                    if (min.length() < val[i].length()) { min=val[i];}
+                        System.out.println("самое длинное число: " + min + " длина которого " + min.length());
+
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/by/it/veselov/JD01_02/Task_1_A/Task_3_A.java
@@ -0,0 +1,31 @@
+package by.it.veselov.JD01_02.Task_1_A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by yegorveselov on 16.02.16.
+ */
+public class Task_3_A {
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+        String rLine = bufferedReader.readLine();
+        bufferedReader.close();
+        String[] val = rLine.split(" ");
+        boolean m=false;
+        for (int i=0; i<val.length; i++){
+            char[] str = val[i].toCharArray();
+            for (int j=0; j<str.length-1;j++) {
+                for (int k = j+1; k < str.length; k++)
+                { if (str[j] == str[k]) {m=true; break;}
+                else {m=false;}
+                }
+                if (m) break;}
+            if (!m) System.out.println(val[i]);
+        }
+    }
+
+}
