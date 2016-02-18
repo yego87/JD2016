b0dfc8b38527b304726734f6673f2670ea8c35d7
yegor.veselov <yegor.veselov@gmail.com>
2016-02-18 17:36:33 +0300

    Добавил решение B! И немного переименовал кллассы в задании А!

--- src/by/it/veselov/JD01_02/Task_1_A/Task_A_4.java
+++ src/by/it/veselov/JD01_02/Task_1_A/Task_A_4.java
@@ -7,25 +7,24 @@ import java.io.InputStreamReader;
 /**
  * Created by yegorveselov on 16.02.16.
  */
-public class Task_3_A {
+public class Task_A_4 {
     public static void main(String[] args) throws IOException {
-
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-        BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String rLine = bufferedReader.readLine();
         bufferedReader.close();
-        String[] val = rLine.split(" ");
-        boolean m=false;
-        for (int i=0; i<val.length; i++){
-            char[] str = val[i].toCharArray();
-            for (int j=0; j<str.length-1;j++) {
-                for (int k = j+1; k < str.length; k++)
-                { if (str[j] == str[k]) {m=true; break;}
-                else {m=false;}
-                }
-                if (m) break;}
-            if (!m) System.out.println(val[i]);
-        }
-    }
 
-}
+        String[] val = rLine.split(" ");
+        boolean b = false;
+        for (int i=0; i<val.length; i++){
+        char[] str = val[i].toCharArray();
+            int j;
+            int k = 0;
+        for ( j = 0; j < str.length / 2; k = str.length - 1, j++, k--){
+                if (str[i]==str[k]){b=true; break;}
+            if (b) break;}
+            if (!b) System.out.println(val[i]);
+
+
+    }
+}}
---
+++ src/by/it/veselov/JD01_02/Task_1_A/Task_B_2.java
@@ -0,0 +1,23 @@
+package by.it.veselov.JD01_02.Task_1_A;
+
+import java.io.IOException;
+
+/**
+ * Created by yegorveselov on 18.02.16.
+ */
+public class Task_B_2 {
+    public static void main(String[] args) throws IOException {
+        int k = 9;
+        int [] array = new int [k];
+        int n = 3;
+        int l=1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++,l++) {
+                System.out.print(l);
+                if( l==k) break;
+            }
+            if (l==k) break;
+            System.out.println();
+        }
+        }}
+
