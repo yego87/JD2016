23b3dbaa03c873c34a4f9056aa4039a678dd1766
Mikhail Darashchonak <mikhail_darashchonak@epam.com>
2016-02-18 14:46:08 +0300

    JD01-08 Draft

---
+++ src/by/it/dorostchenok/jd01_08/DomesticMovie.java
@@ -0,0 +1,7 @@
+package by.it.dorostchenok.jd01_08;
+
+public abstract class DomesticMovie implements Movie{
+    public boolean isPlaying() {
+        return false;
+    }
+}
---
+++ src/by/it/dorostchenok/jd01_08/Movie.java
@@ -0,0 +1,8 @@
+package by.it.dorostchenok.jd01_08;
+
+public interface Movie {
+    String get_title();
+    void Play();
+    void Stop();
+    boolean isPlaying();
+}
