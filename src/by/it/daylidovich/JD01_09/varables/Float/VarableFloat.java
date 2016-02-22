992f52cbbb87bf54f1887bf84942571f72d28b34
Dolf <dmitry.dailidovich@gmail.com>
2016-02-22 18:58:01 +0300

    JD01_09 Float operation and Vector operation work.

--- src/by/it/daylidovich/JD01_09/varables/Float/VarableFloat.java
+++ src/by/it/daylidovich/JD01_09/varables/Float/VarableFloat.java
@@ -1,9 +1,8 @@
 package by.it.daylidovich.JD01_09.varables.Float;
 
 import by.it.daylidovich.JD01_09.interfaces.IVarable;
-import by.it.daylidovich.JD01_09.varables.Varable;
 
-public class VarableFloat extends Varable implements IVarable{
+public class VarableFloat extends OperationFloat implements IVarable{
     private double value = 0;
 
     public VarableFloat(double value){this.value = value;}
