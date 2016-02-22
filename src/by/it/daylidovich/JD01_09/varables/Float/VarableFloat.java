c3705172e6c662f798bb1c8263a38b7fc8d73670
Dolf23 <dmitry.dailidovich@gmail.com>
2016-02-22 12:15:28 +0300

    JD01_09 start.

--- src/by/it/daylidovich/JD01_09/varables/Float/VarableFloat.java
+++ src/by/it/daylidovich/JD01_09/varables/Float/VarableFloat.java
@@ -1,8 +1,9 @@
 package by.it.daylidovich.JD01_09.varables.Float;
 
 import by.it.daylidovich.JD01_09.interfaces.IVarable;
+import by.it.daylidovich.JD01_09.varables.Varable;
 
-public class VarableFloat extends OperationFloat implements IVarable{
+public class VarableFloat extends Varable implements IVarable{
     private double value = 0;
 
     public VarableFloat(double value){this.value = value;}
