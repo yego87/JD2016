48348e8d99c3da07d98019f869f3be3f27b520e5
Alex <375336849110@tut.by>
2016-02-24 07:58:55 +0300

    JD01_11 Примеры из презентации

--- src/by/it/akhmelev/JD01_10/Demo/interfaces/IVar.java
+++ src/by/it/akhmelev/JD01_10/Demo/interfaces/IVar.java
@@ -3,7 +3,7 @@ package by.it.akhmelev.JD01_10.Demo.interfaces;
 
 
 
-public interface IVar {
+public interface IVar extends IAdd,IDiv,IMul,ISub{
     String toString();            //метод вывода переменной в строку
     void setFrom(String str);     //метод чтения значения переменной из строки
 }
