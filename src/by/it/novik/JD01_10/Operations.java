05b4005f59ed5540ddf0288f37b49136d924dac0
Noviko <helloiamkseniya.novik@gmail.com>
2016-02-24 12:35:28 +0300

    JD01_10, start

---
+++ src/by/it/novik/JD01_10/Operations.java
@@ -0,0 +1,10 @@
+package by.it.novik.JD01_10;
+
+
+public interface Operations {
+
+    AVar add(AVar var);
+    AVar sub(AVar var);
+    AVar div(AVar var);
+    AVar mul(AVar var);
+}
---
+++ src/by/it/novik/JD01_10/Runner.java
@@ -0,0 +1,8 @@
+package by.it.novik.JD01_10;
+
+
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
