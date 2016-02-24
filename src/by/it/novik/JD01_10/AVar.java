c9c51a0fd7dd2db602c2543073358b0209ff500a
Noviko <helloiamkseniya.novik@gmail.com>
2016-02-24 12:35:28 +0300

    JD01_10, start

---
+++ src/by/it/novik/JD01_10/AVar.java
@@ -0,0 +1,25 @@
+package by.it.novik.JD01_10;
+
+
+public abstract class AVar implements Operations{
+
+    @Override
+    public AVar add(AVar var) {
+        return null;
+    }
+
+    @Override
+    public AVar sub(AVar var) {
+        return null;
+    }
+
+    @Override
+    public AVar div(AVar var) {
+        return null;
+    }
+
+    @Override
+    public AVar mul(AVar var) {
+        return null;
+    }
+}
