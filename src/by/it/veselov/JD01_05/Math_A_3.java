5374f0813201778d91cec6b2eec87a4a512452dd
yegor.veselov <yegor.veselov@gmail.com>
2016-02-26 11:17:54 +0300

    Добавил математику A и B, так же добавил JD 01_08

--- src/by/it/veselov/JD01_05/Math_A_3.java
+++ src/by/it/veselov/JD01_05/Math_A_3.java
@@ -4,6 +4,15 @@ package by.it.veselov.JD01_05;
 public class Math_A_3 {
     public static void main(String[] args) {
 
-    }
+        double l;
+        double c;
+        double x = 12.1;
+        double y ;
+        for (c = -10; c <=1; c = c + 0.5){
+            l = Math.pow(Math.pow(Math.abs(2*x-c),3.0),1.0/5.0)+0.567;
+            System.out.printf("t=%3.2f \t  l=%1.4f\n",c,l);
+        }
 
-}
+
+
+}}
--- src/by/it/veselov/JD01_05/Math_B_1.java
+++ src/by/it/veselov/JD01_05/Math_B_1.java
@@ -4,4 +4,15 @@ package by.it.veselov.JD01_05;
  * Created by yegorveselov on 18.02.16.
  */
 public class Math_B_1 {
-}
+    public static void main(String[] args) {
+
+        double d=1;
+        double a;
+        for (a = -5; a <=12; a = a + 3.75){
+            for (int t = 1; t <=3; t++){
+                d = d*Math.sin(a - t);
+            System.out.println(d);
+            }
+
+        }
+}}
