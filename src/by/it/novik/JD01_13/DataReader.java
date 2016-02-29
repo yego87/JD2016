4632f6ecc294aa0fd42d9d90c4532d516249a48c
Noviko <helloiamkseniya.novik@gmail.com>
2016-02-29 12:36:44 +0300

    JD01_13, A

--- src/by/it/novik/JD01_13/DataReader.java
+++ src/by/it/novik/JD01_13/DataReader.java
@@ -13,6 +13,7 @@ public class DataReader {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Enter positive integers, please. When done type 'END'");
+        System.out.println("Enter positive integers, please. When done type 'END'");
 
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
                  BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -47,11 +48,32 @@ public class DataReader {
 
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
                  BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                 String rLine = bufferedReader.readLine();
-                 bufferedReader.close();
 
-        String[] str = rLine.split(" ");
+
+        while (true) {
+            String rLine = bufferedReader.readLine();
+            if (rLine.equals("END")) break;
+            try {
+                stringConverter(rLine);
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry, you were asked to enter an integer");
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
+    public static double stringConverter(String str) throws ArithmeticException{
+
+        double i = Double.parseDouble(str);
+        if (i<0) throw new ArithmeticException("Sorry, your number isn't positive. It can't be square-rooted:(");
+        double sqrted = Math.sqrt(i);
+        sum=sum+sqrted;
+        System.out.println("You entered " + i + ". Its square-roots sum is " + sqrted + ". Total square-roots sum is " + sum);
+        return sqrted;
+    }
+
+
 
 }
 
