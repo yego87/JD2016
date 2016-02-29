e9fa80d92c3bc385a6167e669dd16eb18b9aae85
Noviko <helloiamkseniya.novik@gmail.com>
2016-02-29 12:36:44 +0300

    JD01_13, A

--- src/by/it/novik/JD01_13/DataReader.java
+++ src/by/it/novik/JD01_13/DataReader.java
@@ -1,14 +1,12 @@
 package by.it.novik.JD01_13;
 
-import by.it.Main;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
 public class DataReader {
-<<<<<<< de547866f24e8c11c15e4cfa51494a7595255a94
+    public static void main(String[] args) throws IOException {
 
     public static double sum = 0;
 
@@ -17,6 +15,8 @@ public class DataReader {
 
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
                  BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                 String rLine = bufferedReader.readLine();
+                 bufferedReader.close();
 
 
         while (true) {
@@ -44,19 +44,6 @@ public class DataReader {
 
 
 
-=======
-    public static void main(String[] args) throws IOException {
-
-
-        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                 String rLine = bufferedReader.readLine();
-                 bufferedReader.close();
-
-        String[] str = rLine.split(" ");
-    }
-
->>>>>>> JD01_13
 }
 
 
