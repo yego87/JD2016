35d0ff9ad9802243cd5181ff3c3c501625b870ed
Dolf <dmitry.dailidovich@gmail.com>
2016-02-29 19:03:44 +0300

    JD01_12 A3 done.

---
+++ src/by/it/daylidovich/JD01_12/A/task3/SortArray.java
@@ -0,0 +1,33 @@
+package by.it.daylidovich.JD01_12.A.task3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortArray {
+    public static ArrayList<Integer> fillArrayList(int count){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add((int)(Math.random()*20-10));
+        }
+        return list;
+    }
+
+    public static List sortList(List<Integer> list){
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j) < 0 && list.get(j+1) >= 0){
+                    list.set(j, (list.get(j) + list.get(j+1)));
+                    list.set((j+1), (list.get(j) - list.get(j+1)));
+                    list.set(j, (list.get(j) - list.get(j+1)));
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void printList(List<Integer> list){
+        for (int x: list)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+}
