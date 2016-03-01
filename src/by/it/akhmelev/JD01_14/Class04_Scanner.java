b34ae57ad72a066a38ccc3e3afd57415a3b61043
Alex <375336849110@tut.by>
2016-03-02 00:36:45 +0300

    JD01_14 Примеры для теории

--- src/by/it/akhmelev/JD01_14/Class04_Scanner.java
+++ src/by/it/akhmelev/JD01_14/Class04_Scanner.java
@@ -3,7 +3,7 @@ package by.it.akhmelev.JD01_14;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class DemoScanner {
+public class Class04_Scanner {
     public static void main(String[ ] args) {
         double sum = 0.0;
         Scanner scan = new Scanner("1,3;2,0; 8,5; 4,8;9,0; 1; 10;");
