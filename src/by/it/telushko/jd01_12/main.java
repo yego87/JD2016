dc7f6b966668b4afe20768b595a938066e965094
Телушко <vladimir.telushko@gmail.com>
2016-03-03 00:48:43 +0300

    В работе: jd01_10 jd01_12 A+B jd01_13 --- jd01_14 A

--- src/by/it/telushko/jd01_12/main.java
+++ src/by/it/telushko/jd01_12/main.java
@@ -1,16 +1,14 @@
 package by.it.telushko.jd01_12;
 
 
+import by.it.telushko.jd01_02.InOut;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class main {
     public static void main(String[] args){
-        //A
-        Task_A.taskA();
-
-        //В-1;
         String text= "The summer holiday is over. It is time to think of the new school year. " +
                 "And while some are eager to know their new teacher and meet their friends to tell " +
                 "them about all their summer adventures, others are more worried about the new year. Here " +
@@ -23,6 +21,32 @@ public class main {
                 "of sleep and to eat healthy food. This of course needs several days of \"rehabilitation\". The ideal thing is" +
                 " to start reconnecting with a healthy lifestyle one week to ten days before school starts.\n";
 
-        Task_B.task_B(text);
+      //  System.out.println("Выберите тип задач(а, b, c)\n(можно раскомментировать нужную букву)");
+     //   String taskNum=InOut.consoleLineInput(); //выбор типа задания
+        //String taskNum="a";
+        String taskNum="b";
+        //String taskNum="c";
+
+        switch (taskNum){
+            case "a":{Task_A.taskA();break;}
+
+            case "b": {
+               // System.out.println("Введите номер задачи (1 или 2)");
+               // String bTaskNum=InOut.consoleLineInput();
+                String bTaskNum ="2";
+                switch (bTaskNum){
+                    case "1":{Task_B.task_B1(text);break;}
+                    case "2":{Task_B.task_B2(5000);break;}
+                }
+                break;
+            }
+
+            case "c": {
+
+                break;
+            }
+
+            default :{System.out.println("Ошибка!");break;}
+        }
     }
 }
