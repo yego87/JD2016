b1ac21776ccd20a4012bb48e2f2837a6556f2347
Телушко <vladimir.telushko@gmail.com>
2016-03-09 02:26:13 +0300

    Добавлена задача 13C, добавлен finally в задачу 13B Всего сделано: jd01_10 jd01_12 A+B+C1 jd01_13 A+В+C jd01_14 A+B+C jd01_15 A+B(в работе)

--- src/by/it/telushko/jd01_15/Task_B.java
+++ src/by/it/telushko/jd01_15/Task_B.java
@@ -1,22 +1,72 @@
 package by.it.telushko.jd01_15;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task_B {
     public static void taskB1() throws IOException {
         String src = System.getProperty("user.dir")+"/src/by/it/telushko/";
         String inputFilename = src+"jd01_15/Main.java";
+        String outputFilename = src+"jd01_15/Main.txt";
+
+        // cчитываем текст программы в SttingBuilder
+
+        StringBuilder javatext = new StringBuilder();
 
         try (BufferedReader reader=new BufferedReader(new FileReader(inputFilename))){
             String line;
             while ((line=reader.readLine())!=null){
+                javatext.append(line);
+                javatext.append("\n");
+            }
+            reader.close();
+
+        //  паттерны для коментариев и построчного вывода.
+
+            Pattern comment = Pattern.compile("(/\\*\\n(.*\\n)*\\*/)|(//.*)|(/\\*.*\\n(.*\\n)*.*\\*/)");
+            Pattern codeLine = Pattern.compile("(.*\\n)");
+
+
+              //ищем вхождения и пишем их в линкед
+            LinkedList<Integer[]> comments=new LinkedList<Integer[]>();
+            Matcher okComment = comment.matcher(javatext);
+
+            Integer[] find = new Integer[2]; //временный массивсчик для переборов
+
+            while (okComment.find()){
+
+                find[0]=okComment.start();
+                find[1]=okComment.end()-1;
+                comments.add(find);
+                System.out.println(javatext.substring(okComment.start(),okComment.end()));
 
             }
+
+            //посчитали
+            //удаляем
+
+            while(!(comments.isEmpty())){
+
+                find=comments.pollLast();
+                System.out.println(find [0]);
+                System.out.println(find [1]);
+            }
+
+System.out.println(javatext);
+
+            // пишем в файл
+            PrintWriter writer = new PrintWriter(new FileWriter(outputFilename));
+            Matcher okCodeLine = codeLine.matcher(javatext);
+            while (okCodeLine.find()){
+                writer.println(javatext.substring(okCodeLine.start(),okCodeLine.end()));
+            }
+            writer.close();
         }
         catch (IOException e){
             throw new IOException("Ошибка  чтения"+inputFilename,e);
         }
+
     }
 }
