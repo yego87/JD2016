525b8f009f3fdc5ac8682ece88b0de57012681dc
Alex <375336849110@tut.by>
2016-03-09 02:18:23 +0300

    JD02_01 Потоки (примеры)

---
+++ src/by/it/akhmelev/JD02_01/demo/IBuyer.java
@@ -0,0 +1,8 @@
+package by.it.akhmelev.JD02_01.demo;
+
+public interface IBuyer {
+    void enterToMarket();   //вошел в магазин
+    void chooseGoods();     //выбрал товары
+    void goToOut();         //отправился на выход
+}
+
