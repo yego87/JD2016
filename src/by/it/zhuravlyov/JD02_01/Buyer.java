f93d04f6e7b4c29d94469f397406f754ccd3b388
Mihail Soloview <mortr@yandex.ru>
2016-03-09 10:55:30 +0300

    Создали класс и интерфейс и main . Ввели в интерфейс методы класса. override сделали.

---
+++ src/by/it/zhuravlyov/JD02_01/Buyer.java
@@ -0,0 +1,25 @@
+package by.it.zhuravlyov.JD02_01;
+
+/**
+ * Created by user_2 on 09.03.2016.
+ */
+public class Buyer extends Thread implements Runnable, IBuyer {
+
+
+    int num ; //коль-во  покупателей
+
+
+    @Override
+    public void enterToMarket() {
+    }
+
+    @Override
+    public void goToOut() {
+
+    }
+
+    @Override
+    public void chooseGoods() {
+
+    }
+}
