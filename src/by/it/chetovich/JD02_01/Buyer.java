1c8ba92cea391877ca38a1bf1fd70b0501bf4985
Yultos <chatovich@gmail.com>
2016-03-10 10:31:31 +0300

    Удалила свой репозиторий, всё заново

--- src/by/it/chetovich/JD02_01/Buyer.java
+++ src/by/it/chetovich/JD02_01/Buyer.java
@@ -1,17 +1,31 @@
 package by.it.chetovich.JD02_01;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * class for buyers
  */
 public class Buyer extends Thread implements Runnable,IBuyer, IUseBacket {
 
-    int num;
+    private int num;
+    private Backet backet;
+    private boolean retired;
 
-    public Buyer(int num) {
+    public Buyer(int num, boolean retired) {
         this.num = num;
+        this.retired = retired;
         this.setName("Buyer " + num + " ");
         start();
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public Backet getBacket() {
+        return backet;
     }
 
     @Override
@@ -35,16 +49,20 @@ public class Buyer extends Thread implements Runnable,IBuyer, IUseBacket {
     }
 
     @Override
-    public void chooseGoods(){/*
-        try{
-            int goodsQuantity = Rnd.fromTo(1,5);
-            for (int i = 0; i < goodsQuantity; i++) {
-                putGoodsIntoBacket("");
-            }
-        } catch (InterruptedException e) {
-            System.out.println(this+" некорректное завершение ожидания.");
-        }*/
-        System.out.println(this+" chose goods.");
+    public void chooseGoods(){
+
+        Map<String,Integer> buyerBacketMap = new HashMap<>();
+        int goodsQuantity = Rnd.fromTo(1,4);
+        for (int i = 0; i < goodsQuantity; i++){
+
+            String randomGood = Goods.takeRandomGood();
+            putGoodsIntoBacket(randomGood);
+            buyerBacketMap.put(randomGood, Goods.getPrice(randomGood));
+        }
+        backet = new Backet(buyerBacketMap, this.num);
+
+        System.out.print(this+" chose goods, "+(retired?"it took him a while, he is retired. ":"did it fast. "));
+        System.out.println(backet);
     }
 
     @Override
@@ -56,7 +74,7 @@ public class Buyer extends Thread implements Runnable,IBuyer, IUseBacket {
     @Override
     public void takeBacket() {
         try{
-            int pause = Rnd.fromTo(200, 500);
+            int pause = retired?Rnd.fromTo(200, 500):Rnd.fromTo(300, 700);
             Thread.sleep(pause);
         } catch (InterruptedException e) {
             System.out.println(this+" некорректное завершение ожидания.");
@@ -67,7 +85,7 @@ public class Buyer extends Thread implements Runnable,IBuyer, IUseBacket {
     @Override
     public void putGoodsIntoBacket(String good) {
         try{
-            int pause = Rnd.fromTo(2000, 5000);
+            int pause = retired?Rnd.fromTo(100, 200):Rnd.fromTo(150, 300);
             Thread.sleep(pause);
 
         } catch (InterruptedException e) {
--- src/by/it/chetovich/JD02_01/Main.java
+++ src/by/it/chetovich/JD02_01/Main.java
@@ -1,12 +1,8 @@
 package by.it.chetovich.JD02_01;
 
-import by.it.chetovich.Matlab.UtilsMatlab;
-import by.it.chetovich.Matlab.Var;
-import com.sun.jmx.remote.internal.ArrayQueue;
 
-import java.io.File;
 import java.util.ArrayDeque;
-import java.util.Map;
+import java.util.Calendar;
 import java.util.Queue;
 
 /**
@@ -19,19 +15,27 @@ public class Main {
         int countByers = 0;
         Queue<Buyer> queue = new ArrayDeque<>();
 
-        Map<String,Integer> map = Utils02_01.putGoodsFromFileIntoMap();
-        Map<String,Integer> clientsBacket = null;
-
         while (countByers<10){
-            Thread.sleep(1000);
-            int goodsQuantity = Rnd.fromTo(1,5);
-            
+            Calendar c = Calendar.getInstance();
+            int currentSecond = c.get(Calendar.SECOND);
+
+            if (currentSecond>30) Thread.sleep(1000);
+            else Thread.sleep(8000);
+            //(currentSecond>30)?Thread.sleep(1000):Thread.sleep(3000);
+
             int count = Rnd.fromTo(0,2);
             for (int i = 0; i < count; i++) {
                 countByers++;
                 if (countByers<11){
-                    Buyer buyer = new Buyer(countByers);
-                    queue.add(buyer);
+                    if (countByers%4==0) {
+                        Buyer buyer = new Buyer(countByers, true);
+                        queue.add(buyer);
+                    }
+                    else {
+                        Buyer buyer = new Buyer(countByers, false);
+                        queue.add(buyer);
+                    }
+
                 }
             }
 
