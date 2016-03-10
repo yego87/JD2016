21fa1451519b648ced5589554b16823bd69aa84a
Alex <375336849110@tut.by>
2016-03-10 23:29:55 +0300

    JD02_02 Потоки (примеры)

--- src/by/it/akhmelev/JD01_04/Demo.java
+++ src/by/it/akhmelev/JD01_04/Demo.java
@@ -6,5 +6,7 @@ public class Demo  {
     public static void main(String[] args) throws IOException {
         System.out.print("Demo...");
         int[] i= new int[]{1};
+        int check=2>5?1:2;
+
     }
 }
