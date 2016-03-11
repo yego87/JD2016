eac12a2fbdbf6b41891699e5d114982694fa2163
Noviko <helloiamkseniya.novik@gmail.com>
2016-03-11 12:31:42 +0300

    JD02_02

--- src/by/it/novik/JD02_01/Cashier.java
+++ src/by/it/novik/JD02_01/Cashier.java
@@ -1,72 +1,41 @@
 package by.it.novik.JD02_01;
 
+
 import java.util.ArrayList;
 
 public class Cashier extends Thread implements ICashier {
-    private int num;
-    Customer customer;
-    private static Cashier ourInstance = new Cashier();
+    Prices priceList;
 
-    public static Cashier getInstance() {
-        return ourInstance;
-    }
 
-    static int numOfCashiers = 0;
-    static int completeCustomers = 0;
+    public Cashier(Basket basket) {
+        int totalCount = 0;
 
-    private Cashier() {
-        this.num = ++numOfCashiers;//volatile or atomic??
-        this.setName("Кассир N " + num);
-    }
-    public String toString() {
-        return this.getName();
-    }
-
-    static final int planNumOfCustomers = 5;
-
-    public void run() {
-        System.out.println(this.getName() + " открылась");//open cash
-        while(!(completeCustomers>=planNumOfCustomers)) {//пока не выполнен план, т.е. должно быть покупателей больше либо равно как по плану (5)
-            customer = Queue.poll();
-            if (customer != null)
-                synchronized (customer) {
-                    serve(customer);
-                    completeCustomers++;
-
-                    customer.shouldWait = false;//пора отпустить его дальше, т.е. отменить условие + notify
-                    customer.notify();
-                }
-            else {
-                customer.think();
-            }
-        }
-        System.out.println(this.getName() + " закрылась.");
-    }
-
-    @Override
-    public ArrayList<String> getProducts(Basket basket) {
-        return basket.space;
-    }
-
-    @Override
-    public long receivePayment(ArrayList<String> products) {
-        long bill = 0;
-        Prices priceList = Prices.getInstance();
-
-        for (String product : products) {
-            bill += priceList.getPrice(product);
+        for (String product : basket) {
+            totalCount += priceList.getPrice(product);
         }
 
-        return bill;
+        System.out.println(this.getName() + " потратил " + totalCount + " USD");
+    }
+
+
+public void run() {
+    getCustomer();
+    getProducts();
+    receivePayment();
+
+}
+    @Override
+    public void getCustomer() {
+        new Customer("Sam", priceList);
     }
 
     @Override
-    public void serve(Customer customer) {
-        long payment = receivePayment(customer.basket.space);
+    public void getProducts() {
+        new Basket();
+    }
 
-        customer.think();
-
-        System.out.printf("%s заплатил %d USD.\n", customer, payment);
+    @Override
+    public void receivePayment() {
 
     }
 }
