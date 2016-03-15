f6c67b2009c046ff22dee7773382192ebe016702
Dolf <dmitry.dailidovich@gmail.com>
2016-03-15 17:06:09 +0300

    JD02_03 tabular output checks.

--- src/by/it/daylidovich/JD02_03/Main.java
+++ src/by/it/daylidovich/JD02_03/Main.java
@@ -3,20 +3,20 @@ package by.it.daylidovich.JD02_03;
 import by.it.daylidovich.JD02_03.Buyer.Buyer;
 import by.it.daylidovich.JD02_03.Cashier.Manager;
 import by.it.daylidovich.JD02_03.Goods.Goods;
-
-import static by.it.daylidovich.JD02_03.Utils.RandomFromInterval.randomInterval;
+import by.it.daylidovich.JD02_03.Utils.CountBuyersInMoment;
 
 public class Main {
     static int countBuyers = 0;
     public static void main(String[] args) throws InterruptedException {
 
-        int planBuyers = 15;
+        int planBuyers = 10;
         Goods.readGoods();
+        System.out.printf("%-30s%-30s%-30s%-30s%-30s%-15s%-15s\n","Касса №1", "Касса №2", "Касса №3", "Касса №4", "Касса №5", "Длинна очереди", "Выручка магазина");
         Manager manager = new Manager();
         manager.start();
         while (planBuyers > countBuyers){
             Thread.sleep(1000);
-            int countBuyersInOneMoment = randomInterval(0, 2);
+            int countBuyersInOneMoment = CountBuyersInMoment.countBuyersInOneMoment();
             for (int i = 0; i < countBuyersInOneMoment; i++) {
                 countBuyers++;
                 new Buyer(countBuyers);
