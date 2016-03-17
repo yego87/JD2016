4f48bef0951d2d7481b8350fd6886b8812356f4e
Dolf <dmitry.dailidovich@gmail.com>
2016-03-17 17:08:18 +0300

    JD02_04 done saving expression.

--- src/by/it/daylidovich/JD02_03/Buyer/CountBuyersInMoment.java
+++ src/by/it/daylidovich/JD02_03/Buyer/CountBuyersInMoment.java
@@ -1,4 +1,4 @@
-package by.it.daylidovich.JD02_03.Utils;
+package by.it.daylidovich.JD02_03.Buyer;
 
 public class CountBuyersInMoment {
     public static int countBuyersInOneMoment(int countSeconds){
