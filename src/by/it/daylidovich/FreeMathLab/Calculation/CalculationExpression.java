d39a0f1497a3fa0550206952a3f31e91411277e9
Dolf <dmitry.dailidovich@gmail.com>
2016-03-17 18:06:58 +0300

    JD02_04 A done.

--- src/by/it/daylidovich/FreeMathLab/Calculation/CalculationExpression.java
+++ src/by/it/daylidovich/FreeMathLab/Calculation/CalculationExpression.java
@@ -8,27 +8,6 @@ import static by.it.daylidovich.FreeMathLab.InputOutput.Reader.*;
 import static by.it.daylidovich.FreeMathLab.InputOutput.Reader.getVariable;
 
 public class CalculationExpression {
-    public static void processExpression(String stringInput) throws IOException {
-        String operation = readOperation(stringInput);
-        if (!operation.equals("=")){
-            System.out.println(calculationExpression(stringInput));
-        }
-        else
-        {
-            String name = readNameVariable(stringInput);
-            String variable = readExpression(stringInput).trim();
-            if (name != null && variable != null && readOperation(variable) == null)
-                getVariable(variable).save(name);
-            else
-            {
-                if (readOperation(variable) != null){
-                    getVariable(calculationExpression(variable).toString()).save(name);
-                }
-                else
-                    System.out.println("Некоректный ввод.");
-            }
-        }
-    }
 
     public static Variable calculationExpression(String stringInput) throws IOException {
         String operation = readOperation(stringInput);
