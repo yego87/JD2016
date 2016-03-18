0c3dcfc01596e18f21859d5130e1541a392772f6
chatovich <chatovich@gmail.com>
2016-03-18 11:25:21 +0300

    JD02_05 A

---
+++ src/by/it/chetovich/JD02_05/Main.java
@@ -0,0 +1,12 @@
+package by.it.chetovich.JD02_05;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        PrintHello.printHello("", "");
+
+    }
+}
---
+++ src/by/it/chetovich/JD02_05/PrintHello.java
@@ -0,0 +1,19 @@
+package by.it.chetovich.JD02_05;
+
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * print hello in different language
+ */
+public class PrintHello {
+
+    public static void printHello(String language, String country){
+
+        Locale locale = new Locale(language,country);
+        locale.setDefault(Locale.ENGLISH);
+        ResourceBundle resource = ResourceBundle.getBundle("by.it.chetovich.JD02_05.hello", locale);
+        System.out.println(resource.getString("line1"));
+    }
+}
