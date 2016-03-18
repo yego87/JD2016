b1ab609d19d4013bdc83b1db297ff3c50078b3ec
Noviko <helloiamkseniya.novik@gmail.com>
2016-03-18 12:03:46 +0300

    JD02_05, A

--- src/by/it/novik/JD02_05/GreetingA.java
+++ src/by/it/novik/JD02_05/GreetingA.java
@@ -8,42 +8,57 @@ import java.util.Scanner;
 public class GreetingA {
     public static void main(String[] args) {
 
-        System.out.printf("%s%s%s","Выберите язык\n","Choose language\n","Абярыце мову\n");
+        System.out.printf("%s%s%s", "Выберите язык\n", "Choose language\n", "Абярыце мову\n");
         System.out.println("RU/EN/BY >");
 
-        Scanner scanner = new Scanner(System.in);
+
+
+            Locale rus = Locale.getDefault();
+            Locale bel = new Locale("be", "BY");
+            Locale eng = new Locale("en", "US");
+            Locale cur = Locale.getDefault();
+
         String language = "";
-        if (scanner.hasNext()) {
-            language = scanner.nextLine();
+        boolean ok = false;
+        while (!ok) {
+            Scanner scanner = new Scanner(System.in);
+
+            if (scanner.hasNext()) {
+                language = scanner.nextLine();
+            }
+            switch (language) {
+                case "RU":
+                    cur = rus;ok=true;
+                    break;
+                case "ru":
+                    cur = rus;ok=true;
+                    break;
+                case "EN":
+                    cur = eng;ok=true;
+                    break;
+                case "en":
+                    cur = eng;ok=true;
+                    break;
+                case "BY":
+                    cur = bel;ok=true;
+                    break;
+                case "by":
+                    cur = bel;ok=true;
+                    break;
+                default:
+                    System.out.println("You were asked to enter a language");
+                    ok = false;
+
+            }
         }
-        else {
-            System.out.println("You were asked to enter a language");
-        }
 
 
-        Locale rus = Locale.getDefault();
-        Locale bel = new Locale("be", "BY");;
-        Locale eng = new Locale("en", "US");;
-        Locale cur = Locale.getDefault();
+            if (ok) {
+                ResourceBundle rb = ResourceBundle.getBundle("by.it.novik.JD02_05.text", cur);
 
-        switch (language) {
-            case "RU":
-                cur = rus;
-                break;
-            case "EN":
-                 cur = eng;
-                break;
-            case "BY":
-                cur = bel;
-                break;
+                String greeting = rb.getString("key");
 
-        }
-
-
-        ResourceBundle rb = ResourceBundle.getBundle("by.it.novik.JD02_05.text", cur);
-
-        String greeting = rb.getString("key");
-
-        System.out.println(greeting);
+                System.out.println(greeting);
+            }
     }
 }
