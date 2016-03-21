3746a16c35e8cdb2b23513951bf1b965f8147439
vladimir757 <04111985new>
2016-03-21 12:32:43 +0300

    jd02_06 в работе

---
+++ src/by/it/telushko/jd02_06/Run.java
@@ -0,0 +1,20 @@
+package by.it.telushko.jd02_06;
+
+public class Run {
+    public static void main(String[] args){
+        Log log =new Log().getInstance();
+        Log log2 =new Log().getInstance();
+
+        try {int b = 1 / 0;}
+        catch (ArithmeticException e){System.out.println("Деление на  ноль");}
+
+        try{
+            int[] array = new int[3];
+            for (int i = 0; i <6 ; i++) {
+                array[i]=1;
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){System.out.println("Слишком много элементов");}
+        log.getInstance().close();
+    }
+}
