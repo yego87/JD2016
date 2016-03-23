1394b6b9c89bf3942a697b5005a37bbf29ea8423
Mihail Soloview <mortr@yandex.ru>
2016-03-23 10:34:51 +0300

    интерфейс мебель

---
+++ src/by/it/zhuravlyov/JD01_08nasledovanie/Mebel1.java
@@ -0,0 +1,13 @@
+package by.it.zhuravlyov.JD01_08nasledovanie;
+
+/**
+ * Created by home borovai on 02.03.2016.
+ */
+public interface Mebel1 {
+    String getMebelCategory();
+    String getMebelMaterial();
+    String MebelColour();
+    int getMebelHight();
+    int getMebelLeight();
+    boolean getMebelCompile();
+}
